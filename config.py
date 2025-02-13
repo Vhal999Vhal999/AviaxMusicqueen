426aca4b6064aa9a6fd59927b17cae1e760f66f2
Vhal999Vhal999 <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-02-13 15:07:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,11 +39,11 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DOSTON_KI_DUNIYAA")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ONLY_PUBLIC_LINK_SR")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_DPZ_WORLDS_ll")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/queenbotgrup")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", TRUE))
 
 # make your bots privacy from telegra.ph and put your url here 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-AviaxMusic-08-14")
