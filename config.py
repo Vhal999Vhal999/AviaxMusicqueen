19ade8b38360ed5a62e79d6ff8f666c8cb259bbc
Vhal999Vhal999 <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-03-18 11:02:13 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ROYAL_KILLER")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SILENT_LIFELINE")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/II_AAFAT_II")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
