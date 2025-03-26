f0cf013222843310d6be9194721dfc44eccc6d6a
Vhal999Vhal999 <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-03-26 10:50:45 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/ITZ_SHARMA_JI"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/NOT_UR_ZIDDI"),
         ],
     ]
     return buttons
