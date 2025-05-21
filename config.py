8df384ba31fc53f9f6e4972ffb7fe3d7deded8b0
Royalbot213 <143687906+Royalbot213@users.noreply.github.com>
2025-05-21 21:27:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THE_RAINBOW_CHATS")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/FCR_RAVI")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+hyjl0Mp7DzU4NTA1")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+BQLv8g4UgVtmMTI1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
