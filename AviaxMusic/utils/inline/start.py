ddcdc1a704bc87ae21ea839136deebc7618c2d7c
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-07-24 13:10:03 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://files.catbox.moe/ngufvw.mp4"),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/UTKARSHQUIZHUB"),
         ],
     ]
     return buttons
